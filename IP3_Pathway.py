710a2b9b9cab4a52234f8ab4ca3712dbc69c65a9
Axel Sweeny <axelsweeny@gmail.com>
2017-03-05 14:15:04 -0600

    fixed some small import stuff and added a little to the manipulate file

--- IP3_Pathway.py
+++ IP3_Pathway.py
@@ -1,7 +1,10 @@
-import protNet
+from protNet import *
 #proteins[protName] = Protein(active/inactive, {Name of target:[Activate/Deactivate (T/F), { Upstream Effector: Effect }]})
 proteins['CS'] = Protein(True, {'CS': [False, {}]})
 proteins['CH'] = Protein(True, {'GPCR': [False, {'CS': False}]})
 proteins['Hormone'] = Protein(True, {'GPCR': [True, {}]})
 proteins['GPCR'] = Protein(True, {'Ga': [True, {'CH': False, 'Hormone': True}]})
 proteins['Ga'] = Protein(True, {'IP3': [True, {'GPCR': True}]})
+#for a protein with no downstream effectors we instead have [active message,
+#inactive message, upstream effectors and their effects]
+proteins['IP3'] = Protein(True, {'results': ['IP3 is active', 'IP3 is inactive', {'Ga': True}]})
--- manipulatePath.py
+++ manipulatePath.py
@@ -1,4 +1,28 @@
 import IP3_Pathway
+from protNet import *
 
-def setDownstream(prot):
+#takes the upstream protein and the downstream protein
+def setDownstream(upProt):
+    if('results' in upProt._Protein__targets):
+        if(upProt._Protein__current_state == True):
+            print(upProt._Protein__current_state['results'][0])
+        else:
+            print(upProt._Protein__current_state['results'][1])
+    else:
+        for target in upProt._Protein__targets:
+            print(upProt._Protein__targets[target][0])
+            oldstate = proteins[target]._Protein__current_state
+            #set the current state of the downstream target according to the upstream effector
+            proteins[target]._Protein__current_state = upProt._Protein__targets[target][0]
+            #if the current_state of the downstream protein changed
+            if(oldstate != proteins[target]._Protein__current_state):
+                #Continue the cascade!!!
+                setDownstream(proteins[target])
 
+def setUpstream(upProt, downProt):
+    print()
+
+def prompt():
+    print()
+
+setDownstream(proteins['CH'])
