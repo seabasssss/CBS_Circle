59f59671de2d55ce9bf478d7d39c8b2f2d301f8a
Sebastian Swanson <Seabass@x-10-104-65-50.uofm-secure.wireless.umn.edu>
2017-03-09 13:26:04 -0600

    added IP3_Pathway to manipulatePath

--- IP3_Pathway.py
+++ IP3_Pathway.py
@@ -1,10 +1,10 @@
 from protNet import *
 proteins={}
 #proteins[protName] = Protein(active/inactive, {Name of target:[Activate/Deactivate (T/F), { Upstream Effector: Effect }]})
-proteins['CS'] = Protein(True, {'CS': [False, {}]})
-proteins['CH'] = Protein(True, {'GPCR': [False, {'CS': False}]})
+proteins['GenericInhibitorA'] = Protein(True, {'GenericInhibitorA': [False, {}]})
+proteins['GenericInhibitorB'] = Protein(True, {'GPCR': [False, {'GenericInhibitorB': False}]})
 proteins['Hormone'] = Protein(True, {'GPCR': [True, {}]})
-proteins['GPCR'] = Protein(True, {'Ga': [True, {'CH': False, 'Hormone': True}]})
+proteins['GPCR'] = Protein(True, {'Ga': [True, {'GenericInhibitorB': False, 'Hormone': True}]})
 proteins['Ga'] = Protein(True, {'IP3': [True, {'GPCR': True}]})
 #for a protein with no downstream effectors we instead have [active message,
 #inactive message, upstream effectors and their effects]
--- manipulatePath.py
+++ manipulatePath.py
@@ -1,16 +1,26 @@
 import IP3_Pathway
 from protNet import *
 
+#proteins[protName] = Protein(active/inactive, {Name of target:[Activate/Deactivate (T/F), { Upstream Effector: Effect }]})
+proteins['GenericInhibitorA'] = Protein(True, {'GenericInhibitorA': [False, {}]})
+proteins['GenericInhibitorB'] = Protein(True, {'GPCR': [False, {'GenericInhibitorB': False}]})
+proteins['Hormone'] = Protein(True, {'GPCR': [True, {}]})
+proteins['GPCR'] = Protein(True, {'Ga': [True, {'GenericInhibitorB': False, 'Hormone': True}]})
+proteins['Ga'] = Protein(True, {'IP3': [True, {'GPCR': True}]})
+#for a protein with no downstream effectors we instead have [active message,
+#inactive message, upstream effectors and their effects]
+proteins['IP3'] = Protein(True, {'results': ['IP3 is active', 'IP3 is inactive', {'Ga': True}]})
 #takes the upstream protein and the downstream protein
+
 def setDownstream(upProt):
     if('results' in upProt._Protein__targets):
         if(upProt._Protein__current_state == True):
-            print(upProt._Protein__current_state['results'][0])
+            print(upProt._Protein__current_state['results'],upProt._Protein__current_state['results'][0])
         else:
-            print(upProt._Protein__current_state['results'][1])
+            print(upProt._Protein__current_state['results'],upProt._Protein__current_state['results'][1])
     else:
         for target in upProt._Protein__targets:
-            print(upProt._Protein__targets[target][0])
+            print(upProt._Protein__targets[target],upProt._Protein__targets[target][0])
             oldstate = proteins[target]._Protein__current_state
             #set the current state of the downstream target according to the upstream effector
             proteins[target]._Protein__current_state = upProt._Protein__targets[target][0]
@@ -25,4 +35,4 @@ def setUpstream(upProt, downProt):
 def prompt():
     print()
 
-setDownstream(proteins['CH'])
+setDownstream(proteins['GenericInhibitorA'])
